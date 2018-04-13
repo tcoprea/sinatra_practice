ac6732d2db352ed5632148aaa5aa3bc46ca7fb39
Tereza <terezacamelia@gmail.com>
2018-04-13 14:18:52 -0400

    Added httparty requirement

--- server.rb
+++ server.rb
@@ -1,4 +1,11 @@
 require 'sinatra'
+require 'httparty'
+
+get '/party' do
+  url = 'http://example.com'
+  kitty = HTTParty.get(url)
+  kitty.inspect
+end
 
 get '/' do
   return "hello from Heroku."
